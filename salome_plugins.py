8062780777d45fa822c63019b0194343fb698fd2
physici <r.jacob@live.de>
2017-03-29 19:54:45 +0200

    # Issue 21 - Elmer executables not found
    type

--- salome_plugins.py
+++ salome_plugins.py
@@ -297,8 +297,8 @@ def createMesh(context):
             if os.path.exists(fname):
                 os.remove(fname)
             myMesh.ExportUNV(fname)
-            exec = spawn.find_executable('elmergrid')
-            if exec not None:
+            prgm = spawn.find_executable('elmergrid')
+            if prgm not None:
                 try:
                     subprocess.Popen("ELMERGRID 8 2 {0} -autoclean -out {1}".format(fname, path))
                     main.meshDirectory = path
