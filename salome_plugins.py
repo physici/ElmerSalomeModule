d522ea7da088bd5a4628f5ca3e2ebaf8665533ae
physici <r.jacob@live.de>
2017-03-29 16:07:33 +0200

    Update salome_plugins.py

--- salome_plugins.py
+++ salome_plugins.py
@@ -298,7 +298,7 @@ def createMesh(context):
             try:
                 subprocess.Popen("ELMERGRID 8 2 {0} -autoclean -out {1}".format(fname, path))
             except OSError:
-                QtGui.QMessageBox.Critical(None, "File IO error", "fname: {}, path: {}".format(fname, path))
+                QtGui.QMessageBox.about(None, "File IO error", "fname: {}, path: {}".format(fname, path))
                 print fname
                 print path
                 return
